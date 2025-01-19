9bb40ff7096593fadf40e0c9c26ba2cb0ad34494
hacksaw2 <153176276+hacksaw2@users.noreply.github.com>
2025-01-19 18:21:14 +0530

    new changes

--- src/Svgs/HomeSvg.jsx
+++ src/Svgs/HomeSvg.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomeSvg = () => {
   return (
     <div>
-        <svg class="w-10 h-10 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <svg class="w-10 h-10 text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
 </svg>
       
